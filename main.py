cd8462b7cf52aca6d9c71241bd708bc7d624b4bc
Quinn Bastian <jacobqbastian@gmail.com>
2024-02-06 13:58:39 -0700

    Provided Comments

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import pyarrow
 from credential import  username, password
 import random
 
+#Creates a connection to the Azure Database.
 server = 'mysqlserver16.database.windows.net'
 database = 'OPTCG'
 connection_string = 'DRIVER={ODBC Driver 18 for SQL Server};Server='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+password
 conn = odbc.connect(connection_string)
 
+#A query that selects all the information in the database and saves it within the dataset variable.
 sql = '''
 SELECT * FROM [dbo].[Cards]; 
 '''
@@ -16,9 +18,13 @@ cursor = conn.cursor()
 cursor.execute(sql)
 dataset = cursor.fetchall()
 
+#Chooses a random "card" within the dataset.
 random_card = random.choice(dataset)
+
+#Assigns all of the variables with the card information from the randomly selected card.
 card_number, name, cost, power, tag, effect, counter, card_type, is_blocker, is_rush, card_architype, _ = random_card
 
+#Displays the card information in a user friendly way. 
 print("Random Card:\n")
 print("Card Number:", card_number)
 print("Name:", name)
