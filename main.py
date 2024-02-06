ef7ad16caec4ec0a1e7cfe1ff532f19688e47a5e
Quinn Bastian <jacobqbastian@gmail.com>
2024-02-06 11:35:02 -0700

    My First Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import pypyodbc as odbc 
+import pandas as pd
+import pyarrow
+from credential import  username, password
+import random
+
+server = 'mysqlserver16.database.windows.net'
+database = 'OPTCG'
+connection_string = 'DRIVER={ODBC Driver 18 for SQL Server};Server='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+password
+conn = odbc.connect(connection_string)
+
+sql = '''
+SELECT * FROM [dbo].[Cards]; 
+'''
+cursor = conn.cursor()
+cursor.execute(sql)
+dataset = cursor.fetchall()
+
+random_card = random.choice(dataset)
+card_number, name, cost, power, tag, effect, counter, card_type, is_blocker, is_rush, card_architype, _ = random_card
+
+print("Random Card:\n")
+print("Card Number:", card_number)
+print("Name:", name)
+print("Cost:", cost)
+print("Power:", power)
+print("Tag:", tag)
+print("Effect:", effect)
+print("Counter:", counter)
+print("Type:", card_type)
+print("Is Blocker:", is_blocker)
+print("Is Rush:", is_rush)
+print("Architype:", card_architype)
